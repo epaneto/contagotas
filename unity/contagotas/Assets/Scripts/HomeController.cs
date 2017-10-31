7f2e759bbfb9dbf1aae36f35a892e1f5eb023600
epa <epa@sergios-iMac.local>
2017-10-31 17:27:07 -0200

    trying transitions

--- unity/contagotas/Assets/Scripts/HomeController.cs
+++ unity/contagotas/Assets/Scripts/HomeController.cs
@@ -7,6 +7,7 @@ using DG.Tweening;
 public class HomeController : MonoBehaviour {
 
 	public Button signButton;
+	public Button facebookButton;
 	private string destinySceneName;
 
 	void Start () {
@@ -20,7 +21,18 @@ public class HomeController : MonoBehaviour {
 
 	private void HideScene()
 	{
-		signButton.transform.DOScale(0, 0.6f).SetEase(Ease.OutQuad).OnComplete(GoToScene);
+		GameObject background = GameObject.Find ("background");
+		GameObject logo = GameObject.Find ("logo");
+		GameObject caixalogo = GameObject.Find ("caixa_logo");
+
+		signButton.transform.DOMoveY(-Screen.height - signButton.transform.localScale.y , 0.6f).SetEase(Ease.InQuad).OnComplete(GoToScene);
+		facebookButton.transform.DOMoveY(-Screen.height - facebookButton.transform.localScale.y , 0.6f).SetEase(Ease.InQuad).OnComplete(GoToScene);
+
+		logo.transform.DOMoveY(-Screen.height - logo.transform.localScale.y , 1f).SetEase(Ease.InQuad).OnComplete(GoToScene);
+		caixalogo.transform.DOMoveY(-Screen.height - caixalogo.transform.localScale.y , 1f).SetEase(Ease.InQuad).OnComplete(GoToScene);
+
+		//background.gameObject.GetComponent<Image> ().DOFade (0, 2.0f);
+		background.transform.DOMoveY(-Screen.height / 2 , 1.5f).SetEase(Ease.InQuad).OnComplete(GoToScene);
 	}
 
 	public void CallForScene(string sceneName)
--- unity/contagotas/Assets/Scripts/SignUpController.cs
+++ unity/contagotas/Assets/Scripts/SignUpController.cs
@@ -1,34 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class SignUpController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	private string destinySceneName;
 
-	private void show()
+	void Start()
 	{
-
+		Show ();
 	}
 
-	private void hide()
+	private void Show()
+	{
+		GameObject title = GameObject.Find ("title");
+		GameObject inputname = GameObject.Find ("input_name");
+		GameObject inputmail = GameObject.Find ("input_email");
+		GameObject dropstate = GameObject.Find ("drop_state");
+		GameObject dropcity = GameObject.Find ("drop_city");
+		GameObject requiredfield = GameObject.Find ("required");
+		GameObject btConfirm = GameObject.Find ("bt_confirmar");
+
+
+		title.transform.DOMoveY(2000, 1.2f).SetEase(Ease.OutQuad).From();
+		inputname.transform.DOMoveY(2000, 1.1f).SetEase(Ease.OutQuad).From();
+		inputmail.transform.DOMoveY(2000, 1.0f).SetEase(Ease.OutQuad).From();
+		dropstate.transform.DOMoveY(2000, 0.9f).SetEase(Ease.OutQuad).From();
+		dropcity.transform.DOMoveY(2000, 0.8f).SetEase(Ease.OutQuad).From();
+		requiredfield.transform.DOMoveY(2000, 0.7f).SetEase(Ease.OutQuad).From();
+		btConfirm.transform.DOMoveY(2000, 0.6f).SetEase(Ease.OutQuad).From();
+	}
+
+	private void HideScene()
 	{
 
 	}
 
 	public void CallForScene(string sceneName)
 	{
-		//SceneController.sceneController.BeforeSceneUnload += hide;
-		//SceneController.sceneController.AfterSceneLoad += show;
+		destinySceneName = sceneName;
+		HideScene ();
+	}
 
-		SceneController.sceneController.FadeAndLoadScene (sceneName);
+	private void GoToScene()
+	{
+		SceneController.sceneController.FadeAndLoadScene (destinySceneName);
 	}
 }
