0369da9c0cc5a046b7dbf5618d5a52db1c5590c2
epa <epa@sergios-iMac.local>
2017-11-29 10:43:22 -0200

    game 5 not finished yet

--- unity/contagotas/Assets/Scripts/Games/DragableObject.cs
+++ unity/contagotas/Assets/Scripts/Games/DragableObject.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DragableObject : MonoBehaviour {
 
@@ -13,28 +14,45 @@ public class DragableObject : MonoBehaviour {
 	}
 
 	// Update is called once per frame
+	bool isDraging = false;
 	void Update () {
+		
 		if (isTouchingDestiantion)
 			return;
-		
-		if (Input.GetMouseButton (0)) {
 
-			Transform newTransform = this.gameObject.transform;
-			newTransform.position = Input.mousePosition;
-			this.gameObject.transform.position = newTransform.position;
-
-			Renderer thisRenderer;
-			Renderer destinyRenderer;
-
-			thisRenderer = this.gameObject.renderer;
-			destinyRenderer = destinationObject.gameObject.renderer;
-
-			if (thisRenderer.bounds.Intersects(destinyRenderer.bounds)) {
-				Debug.Log ("TOUCHED");
-				isTouchingDestiantion = true;
-			}
-
+		if (isDraging) {
+			Debug.Log ("DRAG ME!");
+			Transform newSpongeTransform = this.gameObject.transform;
+			newSpongeTransform.position = Input.mousePosition;
 		}
 
+		RaycastHit2D hitInfo = Physics2D.Raycast (new Vector2 (Camera.main.ScreenToWorldPoint (Input.mousePosition).x, Camera.main.ScreenToWorldPoint (Input.mousePosition).y), Vector2.zero, 0);
+
+		if (Input.GetMouseButtonDown (0)){
+			Debug.Log (hitInfo);
+
+			if (hitInfo) {
+				Debug.Log ("its a hit");
+				isDraging = true;
+			}
+		}
+
+		if (Input.GetMouseButtonUp (0)) {
+			isDraging = false;
+		}
+
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+
+		if (coll.gameObject.tag == "dragable")
+		{
+			Physics2D.IgnoreCollision(coll.gameObject.GetComponent<BoxCollider2D>(), this.gameObject.GetComponent<BoxCollider2D>());
+			return;
+		}
+
+
+		Debug.Log ("tocou em algum role");
+		isTouchingDestiantion = true;
 	}
 }
--- unity/contagotas/Assets/Scripts/Games/MachineDragGame.cs
+++ unity/contagotas/Assets/Scripts/Games/MachineDragGame.cs
@@ -13,7 +13,7 @@ public class MachineDragGame : MonoBehaviour {
 	MiniGameDefaultBehavior mdb;
 	Vector3 lastMouseCoordinate = Vector3.zero;
 	bool isPlaying = true;
-	bool allDishesClean = false;
+	bool allClothClean = false;
 
 	// Use this for initialization
 	void Start () {
@@ -28,55 +28,30 @@ public class MachineDragGame : MonoBehaviour {
 		if (!isPlaying)
 			return;
 
-		if (!mdb.hasTimeLeft() && !allDishesClean) {
+		if (!mdb.hasTimeLeft() && !allClothClean) {
 			isPlaying = false;
 			mdb.EndedGameLose ();
 			return;
 		}
 
-		RaycastHit2D hitInfo = Physics2D.Raycast (new Vector2 (Camera.main.ScreenToWorldPoint (Input.mousePosition).x, Camera.main.ScreenToWorldPoint (Input.mousePosition).y), Vector2.zero, 0);
 
-		if (Input.GetMouseButton (0)) {
-
-			Vector3 mouseDelta = Input.mousePosition - lastMouseCoordinate;
-			lastMouseCoordinate = Input.mousePosition;
-
-			if (hitInfo) {
-				
-				if (mouseDelta.x > 5 || mouseDelta.y > 5) {
-					GameObject colliderObject = hitInfo.collider.gameObject;
-					Image colliderImage = colliderObject.GetComponent<Image> ();
-					Color c = colliderImage.color;
-					Debug.Log (c.a);
-					if (c.a > 0) {
-						Debug.Log ("lower alpha");
-						c.a -= 0.03f;
-						colliderImage.color = c;
-
-						float totalAlpha = 0;
-						for (int i = 0; i < dishes.Count; i++) {
-							GameObject dish = dishes [i];
-							Image dishImage = dish.transform.GetComponent<Image> ();
-							totalAlpha += dishImage.color.a;
-						}
-
-						if (totalAlpha <= 0) {
-							isPlaying = false;
-							allDishesClean = true;
-							EndGame ();
-						}
-					}
-				}
-
-
+		allClothClean = true;
+		for (int i = 0; i < dishes.Count; i++) {
+			
+			DragableObject obj = dishes [i].GetComponent<DragableObject> ();
+			if (obj.isTouchingDestiantion == false) {
+				allClothClean = false;
+				break;
 			}
-		} 
+		}
+
+		if (allClothClean)
+			EndGame ();
 	}
 
 	void EndGame()
 	{
-		//faucet.AnimationState.Complete -= EndGame;
-
+		isPlaying = false;
 		mdb.EndedGameWin (mdb.maxScore - (mdb.maxScore * mdb.getTimeProgress()));
 	}
 }
