1b5dd96475b0361a5f3de2836dd0506ca46891d4
epa <epa@sergios-iMac.local>
2017-11-29 08:49:38 -0200

    starting setup for game5

---
+++ unity/contagotas/Assets/Scripts/Games/DragableObject.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragableObject : MonoBehaviour {
+
+	public GameObject destinationObject;
+	public bool isTouchingDestiantion = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (isTouchingDestiantion)
+			return;
+		
+		if (Input.GetMouseButton (0)) {
+
+			Transform newTransform = this.gameObject.transform;
+			newTransform.position = Input.mousePosition;
+			this.gameObject.transform.position = newTransform.position;
+
+			Renderer thisRenderer;
+			Renderer destinyRenderer;
+
+			thisRenderer = this.gameObject.renderer;
+			destinyRenderer = destinationObject.gameObject.renderer;
+
+			if (thisRenderer.bounds.Intersects(destinyRenderer.bounds)) {
+				Debug.Log ("TOUCHED");
+				isTouchingDestiantion = true;
+			}
+
+		}
+
+	}
+}
