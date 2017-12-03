b9f0f7e457cf877c5cb3ce5556589a80def63611
epa <epa@sergios-iMac.local>
2017-12-03 18:52:48 -0200

    game 12

---
+++ unity/contagotas/Assets/Scripts/Games/CollectRainGame.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectRainGame : MonoBehaviour {
+    
+    float dropInterval = 0.5f;
+    public GameObject dropParent;
+    MiniGameDefaultBehavior mdb;
+    bool isPlaying = true;
+
+    int neededDrops = 12;
+    int collectedDrops = 0;
+
+	// Use this for initialization
+	void Start () {
+        mdb = this.gameObject.GetComponent<MiniGameDefaultBehavior>();
+        InvokeRepeating("launchDrop", 3.0f, dropInterval);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(!mdb.gameStarted)
+
+            return;
+
+        if (!isPlaying)
+            return;
+        
+        if (!mdb.hasTimeLeft() && collectedDrops < neededDrops)
+        {
+            isPlaying = false;
+            mdb.EndedGameLose();
+            return;
+        }
+	}
+
+    void launchDrop()
+    {
+        GameObject drop = Instantiate(Resources.Load("drop", typeof(GameObject)), new Vector3(Random.Range(-400,400),-220,0), dropParent.transform.rotation) as GameObject;
+        drop.transform.SetParent(dropParent.transform, false);
+    }
+
+    public void CollectedDrop()
+    {
+        collectedDrops++;
+
+        if(collectedDrops >= neededDrops)
+        {
+            EndGame();
+        }
+
+    }
+
+    void EndGame()
+    {
+        isPlaying = false;
+        mdb.EndedGameWin(mdb.maxScore - (mdb.maxScore * mdb.getTimeProgress()));
+    }
+}
---
+++ unity/contagotas/Assets/Scripts/Games/Drop.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drop : MonoBehaviour {
+
+    CollectRainGame controller;
+	// Use this for initialization
+	void Start () {
+        controller = FindObjectOfType<CollectRainGame>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        this.gameObject.transform.Translate(Vector3.down * 23.0f);
+
+        if (this.gameObject.transform.position.y < -1500)
+        {
+            Debug.Log("gota saiu fora");
+            Destroy(this.gameObject);
+        }
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("DragDestination"))
+        {
+            Debug.Log("GOTA TOCOU");
+            controller.CollectedDrop();
+            Destroy(this.gameObject);
+        }
+    }
+}
