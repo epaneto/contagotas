762d283d5ba6e22feaf54cd363a3d214bdd0ae57
epa <epa@sergios-iMac.local>
2017-12-15 23:48:06 -0200

    checkbox at endgame

--- unity/contagotas/Assets/Scripts/MinigamesController.cs
+++ unity/contagotas/Assets/Scripts/MinigamesController.cs
@@ -30,6 +30,9 @@ public class MinigamesController : MonoBehaviour {
     GameObject challengeTxt;
     GameObject endBackground;
 
+    GameObject toogleButton;
+    Toggle toogleComponent;
+
 
     GameObject loseGame;
 	GameObject loseGameSadChar;
@@ -51,6 +54,8 @@ public class MinigamesController : MonoBehaviour {
         hintTxt = GameObject.Find("hint_txt");
         continueButton = GameObject.Find("bt_continuar_score");
         endBackground = GameObject.Find("endgamebackground");
+        toogleButton = GameObject.Find("routine_toogle");
+        toogleComponent = toogleButton.GetComponent<Toggle>();
 
 		scoreTxt = GameObject.Find ("score_txt");
         endGametxt = scoreTxt.GetComponent<Text>();
@@ -100,8 +105,18 @@ public class MinigamesController : MonoBehaviour {
         challengeTxt.transform.DOScale(new Vector3(0, 0, 1), 1.0f).SetEase(Ease.OutBack).From();
         hintTxt.transform.DOScale(new Vector3(0, 0, 1), 1.1f).SetEase(Ease.OutBack).From();
         continueButton.transform.DOScale(new Vector3(0, 0, 1.2f), 1.0f).SetEase(Ease.OutBack).From();
+        toogleButton.transform.DOScale(new Vector3(0, 0, 1.2f), 1.1f).SetEase(Ease.OutBack).From();
         endBackground.transform.DOMoveY(-1400,0.6f).SetEase(Ease.OutQuad).From();
 
+        ///CHECK ACTIVITY STATUS
+        //Debug.Log("prefs is for mission" + minigameIndex + " : " + PlayerPrefs.HasKey("mission" + minigameIndex));
+        toogleComponent.isOn = false;
+        if (PlayerPrefs.HasKey("mission" + minigameIndex))
+        {
+            toogleComponent.isOn = true;
+        }
+        
+            
 		///REMOVE ACTIVE MINIGAME FROM SCREEN
 		JToken sceneName = Missions[minigameIndex]["sceneid"];
         SceneManager.UnloadSceneAsync(sceneName.ToString());
@@ -194,6 +209,20 @@ public class MinigamesController : MonoBehaviour {
         endGame.SetActive(false);
         loseGame.SetActive(false);
 
+        ///save activity status
+        if (toogleComponent.isOn)
+        {
+            Debug.Log("is on");
+            PlayerPrefs.SetString("mission" + minigameIndex, "true");
+        }
+        else
+        {
+            Debug.Log("is off");
+            PlayerPrefs.DeleteKey("mission" + minigameIndex);
+        }
+
+        PlayerPrefs.Save();
+        
         if (minigameIndex + 1 < Missions.Count)
         {
             //Debug.Log ("play next minigame " + minigameIndex);
