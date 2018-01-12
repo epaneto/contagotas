1698efb49b3198c68ad04f612f5ebd2a52825cb3
joao <joaobergamo@gmail.com>
2018-01-11 23:27:24 -0200

    Finished Game 9 - Bucket with holes.

---
+++ unity/contagotas/Assets/Scripts/Games/Hole.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hole : MonoBehaviour {
+
+	TapHoleGame controller;
+
+	// Use this for initialization
+	void Start () {
+		controller = FindObjectOfType<TapHoleGame>();
+	}
+	
+	public void TappedHole()
+	{
+		controller.updateHoles ();
+		this.gameObject.SetActive (false);
+	}
+}
--- unity/contagotas/Assets/Scripts/Games/TapHoleGame.cs
+++ unity/contagotas/Assets/Scripts/Games/TapHoleGame.cs
@@ -1,1 +1,69 @@
-﻿using System.Collections;using System.Collections.Generic;using UnityEngine;public class TapHoleGame : MonoBehaviour {    public List<GameObject> holes;    MiniGameDefaultBehavior mdb;    bool isPlaying = true;    int numHoles = 0;    // Use this for initialization    void Start () {        mdb = this.gameObject.GetComponent<MiniGameDefaultBehavior>();        numHoles = holes.Count;    }		// Update is called once per frame	void Update () {        if (!mdb.gameStarted)            return;        if (!isPlaying)            return;        if (!mdb.hasTimeLeft() && numHoles > 0)        {            isPlaying = false;            mdb.EndedGameLose();            return;        }    }    public void updateHoles()    {        numHoles--;        Debug.Log("update holes " + numHoles);        GameSound.gameSound.PlaySFX("tap");        if (numHoles == 0)            EndGame();    }    void EndGame()    {        mdb.EndedGameWin(mdb.maxScore - (mdb.maxScore * mdb.getTimeProgress()));    }}
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TapHoleGame : MonoBehaviour {
+    public List<GameObject> holes;
+    MiniGameDefaultBehavior mdb;
+    bool isPlaying = true;
+	int holesTapped = 1;
+    int numHolesToTap = 0;
+
+    // Use this for initialization
+    void Start () {
+        mdb = this.gameObject.GetComponent<MiniGameDefaultBehavior>();
+        numHolesToTap = 15;
+		int holeIndex = Random.Range (0, holes.Count);
+		holes [holeIndex].SetActive (true);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (!mdb.gameStarted)
+            return;
+
+        if (!isPlaying)
+            return;
+
+        if (!mdb.hasTimeLeft() && numHolesToTap > 0)
+        {
+            isPlaying = false;
+            mdb.EndedGameLose();
+            return;
+        }
+    }
+
+    public void updateHoles()
+    {
+        numHolesToTap--;
+		holesTapped++;
+		if(holesTapped <= 5)
+			RandomizeHoles (holesTapped);
+        
+		Debug.Log("update holes " + numHolesToTap);
+
+        GameSound.gameSound.PlaySFX("tap");
+
+        if (numHolesToTap == 0)
+            EndGame();
+    }
+
+	private void RandomizeHoles(int numberOfHoles)
+	{
+		while(numberOfHoles != 0)
+		{
+			int holeIndex = Random.Range (0, holes.Count);
+
+			if(!holes[holeIndex].activeSelf)
+			{
+				holes [holeIndex].SetActive (true);
+				numberOfHoles--;
+			}
+		}
+	}
+
+    void EndGame()
+    {
+        mdb.EndedGameWin(mdb.maxScore - (mdb.maxScore * mdb.getTimeProgress()));
+    }
+}
