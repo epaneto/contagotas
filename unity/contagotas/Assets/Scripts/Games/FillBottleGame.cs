7b21e4c122d5233c0b28b155863f9d46803f80dd
joao <joaobergamo@gmail.com>
2018-01-13 23:17:06 -0200

    Minigame Game7 - Oil fill- finished

--- unity/contagotas/Assets/Scripts/Games/FillBottleGame.cs
+++ unity/contagotas/Assets/Scripts/Games/FillBottleGame.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 using Spine;
 using Spine.Unity;
 using Spine.Unity.Modules.AttachmentTools;
+using DG.Tweening;
 
 public class FillBottleGame : MonoBehaviour {
 
-	public GameObject fillObject;
+
+	public GameObject[] oilObjects;
+	public GameObject[] bottleObjects;
+
+	private GameObject fillObject;
+	private int fillOBjectIndex = 0;
 	public GameObject panObject;
 	MiniGameDefaultBehavior mdb;
 	bool isPlaying = true;
@@ -20,6 +26,7 @@ public class FillBottleGame : MonoBehaviour {
 	void Start () {
 		mdb = this.gameObject.GetComponent<MiniGameDefaultBehavior> ();
 		panGraph = panObject.GetComponent<SkeletonGraphic> ();
+		fillObject = oilObjects [fillOBjectIndex];
 	}
 	
 	// Update is called once per frame
@@ -63,9 +70,26 @@ public class FillBottleGame : MonoBehaviour {
 		panGraph.AnimationState.SetAnimation(0,"off",false);
 
 		if (fillObject.transform.localScale.y > minOilFill && fillObject.transform.localScale.y < maxOilFill) {
-			isPlaying = false;
-			EndGame ();
+			
+			if (fillOBjectIndex < 2) {
+				TranslateBottle ();
+				fillOBjectIndex++;
+				fillObject = oilObjects [fillOBjectIndex];
+			} else {
+				isPlaying = false;
+				EndGame ();
+			}
 		}
+	}
+
+	private void TranslateBottle()
+	{
+		oilObjects [0].transform.DOMoveX (oilObjects [0].transform.position.x - 3, 1f);
+		bottleObjects [0].transform.DOMoveX (bottleObjects [0].transform.position.x - 3, 1f);
+		oilObjects [1].transform.DOMoveX (oilObjects [1].transform.position.x - 3, 1f);
+		bottleObjects [1].transform.DOMoveX (bottleObjects [1].transform.position.x - 3, 1f);
+		oilObjects [2].transform.DOMoveX (oilObjects [2].transform.position.x - 3, 1f);
+		bottleObjects [2].transform.DOMoveX (bottleObjects [2].transform.position.x - 3, 1f);
 	}
 
 	void EndGame()
