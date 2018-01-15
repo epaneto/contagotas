9686dcf25c72d9c231ea9a5907ea02f39ad01d58
joao <joaobergamo@gmail.com>
2018-01-15 21:35:13 -0200

    Fixing Game 17 as Epa asked for.

--- unity/contagotas/Assets/Scripts/Games/TouchRotate.cs
+++ unity/contagotas/Assets/Scripts/Games/TouchRotate.cs
@@ -4,28 +4,25 @@ using UnityEngine;
 
 public class TouchRotate : MonoBehaviour {
     public bool isRight;
+	public bool canRotate = false;
 	// Use this for initialization
 	void Start () {
        
         int randomizeAngle = (int)Random.Range(1, 3);
         this.gameObject.transform.Rotate(new Vector3(0, 0, randomizeAngle * 90));
-
+		this.enabled = false;
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
     public void RotateMe()
     {
-		if (!this.isActiveAndEnabled)
+		if (!canRotate) {
+			Debug.Log ("Returning rotate click");	
 			return;
-		
+		}
         //this.gameObject.transform.rotation = Quaternion.Euler(0, 0, 90);
         GameSound.gameSound.PlaySFX("tap");
         this.gameObject.transform.Rotate(new Vector3(0,0,90));
-
+		Debug.Log ("ROTATED");
         int angle = (int)Mathf.Round(this.gameObject.transform.rotation.eulerAngles.z);
 
         if ( angle == 0 ){
